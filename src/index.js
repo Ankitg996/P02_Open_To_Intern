e103a5a7db57cf4bf2b6fda6591e127701715bd3
Ankitg996 <ankitg996@gmail.com>
2022-05-04 13:32:37 +0530

    first update

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const route = requrie('./routes/route.js')
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+
+mongoose.connect("mongodb+srv://ankitg99641:mongo123@cluster0.zdrae.mongodb.net/ProjectDB-02?retryWrites=true&w=majority",{useNewUrlParser: true})
+
+.then(()=>console.log("mongoDB is connected"))
+.catch(err => console.log(err))
+
+app.use('/', route)
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express app running on port"+(process.env.PORT || 3000 ))
+});
