5f61b066d45a60c647f6b0b45e6bd442d0570f65
Ankitg996 <ankitg996@gmail.com>
2022-05-05 16:51:59 +0530

    update 04

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -1,6 +1,7 @@
 const InternModel = require('../models/internModel')
 const CollegeModel = require('../models/collegeModel')
 
+
 const interns = async function (req, res) {
     try {
         let data = req.body
@@ -14,19 +15,23 @@ const interns = async function (req, res) {
 }
 
 const getData = async function (req, res) {
-    let data = req.query.name;
-    
-    let coId = await CollegeModel.find({ name: data }).select({ _id: 1, name: 1, fullName: 1, logoLink: 1 })
-    console.log(coId)
-    
-    if (coId.length == 0) { res.status(404).send({ status: false, data: "No such College Name found" }) }
+    try {
+        let data = req.query.name;
 
-    let intern = await InternModel.find({collegeId:coId}).select({ _id: 1, name: 1, email: 1, moblie_number: 1 })
-    console.log(intern)
-    let interest = [];
-    interest = interest.concat(intern)
-    res.send({ data: coId, interest })
+        let coId = await CollegeModel.find({ name: data }).select({ _id: 1, name: 1, fullName: 1, logoLink: 1 })
+        console.log(coId)
 
+        if (coId.length == 0) { res.status(404).send({ status: false, data: "No such College Name found" }) }
+
+        let intern = await InternModel.find({ collegeId: coId }).select({ _id: 1, name: 1, email: 1, moblie_number: 1 })
+        console.log(intern)
+        let interest = [];
+        interest = interest.concat(intern)
+        res.send({ data: coId, interest })
+    }
+    catch {
+        res.status(400).send({ status: false, message: "Bad Request!" })
+    }
 }
 
 
