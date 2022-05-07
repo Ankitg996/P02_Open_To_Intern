bd488a5050221f7b7d7dbca2e2aaf7a587e28b5b
Ankitg996 <ankitg996@gmail.com>
2022-05-08 04:29:39 +0530

    up

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -9,29 +9,29 @@ const colleges = async function (req, res) {
         let data = req.body
 
         //---------------validation and more-------------------
-        if(!data.name){
-           return res.status(400).send({status:false, message:"college Abbriviation name is mandatory"})          
+        if (!data.name) {
+            return res.status(400).send({ status: false, message: "college Abbriviation name is mandatory" })
         }
-        let abbNameFind = await CollegeModel.findOne({name: data.name})
-        if(abbNameFind){
-            return res.status(400).send({status:false, message:"Douplicate college Abbriviation name, not Allowed!"})
+        let abbNameFind = await CollegeModel.findOne({ name: data.name })
+        if (abbNameFind) {
+            return res.status(400).send({ status: false, message: "Douplicate college Abbriviation name, not Allowed!" })
         }
-        if(!regType01.test(data.name)){        
-            return res.status(400).send({status:false, message:"A valid college Abbriviation name is needed"})
+        if (!regType01.test(data.name)) {
+            return res.status(400).send({ status: false, message: "A valid college Abbriviation name is needed" })
         }
-        if(!data.fullName){
-            return res.status(400).send({status:false, message:"College's full name is mandatory"})
+        if (!data.fullName) {
+            return res.status(400).send({ status: false, message: "College's full name is mandatory" })
         }
-        if(!data.logoLink){
-            return res.status(400).send({status:false, message:"logo link is mandatory"})
+        if (!data.logoLink) {
+            return res.status(400).send({ status: false, message: "logo link is mandatory" })
         }
 
         // ------------------creating Data------------------
         let createCollege = await CollegeModel.create(data)
         res.status(201).send({ Status: true, data: createCollege })
-        
+
     }
-    catch(err){
+    catch (err) {
         res.status(400).send({ status: false, Error: err.message })
     }
 }
--- src/routes/route.js
+++ src/routes/route.js
@@ -4,8 +4,3 @@ const router = express.Router()
 const CollegeController = require('../controllers/collegeController')
 const InternController= require('../controllers/internController')
 
-router.post('/functionup/colleges',CollegeController.colleges)
-router.post('/functionup/interns', InternController.interns)
-router.get('/getAllIntern', InternController.getData)
-
-module.exports = router
