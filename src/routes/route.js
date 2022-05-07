5f52b317f0b1fe870c5de2e199bc103366822893
Ankitg996 <ankitg996@gmail.com>
2022-05-08 04:31:29 +0530

    up

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,3 +4,8 @@ const router = express.Router()
 const CollegeController = require('../controllers/collegeController')
 const InternController= require('../controllers/internController')
 
+router.post('/functionup/colleges',CollegeController.colleges)
+router.post('/functionup/interns', InternController.interns)
+router.get('/functionup/collegeDetails', InternController.getData)
+
+module.exports = router
